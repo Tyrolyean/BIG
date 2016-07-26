c106ab0b29f28c49a697380e94d53f6d4e870b6b
Tyrolyean1 <daniel.plank.dani@gmail.com>
2016-07-26 18:32:34 +0200

    INVENTORY FIXED
    I found a way to save the whole Inventory! YEah!

--- server_outer_part/src/server_outer_part/inventory.java
+++ server_outer_part/src/server_outer_part/inventory.java
@@ -1,6 +1,7 @@
 package server_outer_part;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,18 +14,21 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+import serializers.ItemSerializer;
+
 
 
 public class inventory {
 
 	public static void save_Inventory(Player player){
 		Inventory inventory=player.getInventory();
-		Gson gson =new Gson();
 
 		
 		
 		if(Person_splitter.debug){
-			Person_splitter.logger.log(Level.INFO, "Saving Inventory from "+player.getDisplayName()+" as "+gson.toJson(inventory.getContents()));
+			Person_splitter.logger.log(Level.INFO, "Saving Inventory from "+player.getDisplayName()+" as ");
 		}
 		if (Person_splitter.debug) {
 			System.out.println("Setting file Inventory for Player " + player.getUniqueId().toString());
@@ -40,8 +44,26 @@ public class inventory {
 			e.printStackTrace();
 		}
 		try {
-			FileWriter writer = new FileWriter(file);
-			gson.toJson(inventory, writer);
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			for(ItemStack item:inventory.getContents()){
+				try{
+					if(ItemSerializer.serializeItem(item, false, 0)==null){
+						writer.write("");
+					}else{
+						Gson gson =new Gson();
+						writer.write(gson.toJson(ItemSerializer.serializeItem(item, false, 0)));
+
+					}
+				writer.newLine();
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+			
+			
+			writer.flush();
+			writer.close();
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -49,16 +71,26 @@ public class inventory {
 	}
 	
 	public static void load_Inventory(Player player){
-		String inventory_string=new String();//Initializing for the win!
 		Inventory inventory=player.getInventory();
-		Gson gson =new Gson();
+		ItemStack[] contents=new ItemStack[inventory.getContents().length];
 		inventory.clear();
 		File file = new File(System.getProperty("user.dir") + "/plugins/BIG/inventorys/"
 				+ player.getLocation().getWorld().getName() + "/"
 				+ player.getUniqueId().toString());
 		try {
+			JsonParser parser=new JsonParser();
 			BufferedReader reader =new BufferedReader(new FileReader(file));
-			inventory_string=reader.readLine();
+			for(int i=0;i<contents.length;i++){
+				try{
+				String line=reader.readLine();
+				if(!line.equalsIgnoreCase(null)){
+		            contents[i]=ItemSerializer.deserializeItem(parser.parse(line).getAsJsonObject());
+				}
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+
 			reader.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -66,9 +98,8 @@ public class inventory {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		ItemStack[] content=	gson.fromJson(inventory_string, ItemStack[].class);	
 		
-		inventory.setContents(content);
+		inventory.setContents(contents);
 	}
 	
 	
